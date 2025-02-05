17efedd4beec886b8c2ec20049e39cdc32ce3a66
amayaschalmers <amary@student.chalmers.se>
2025-02-05 08:21:15 +0100

    Create LongTruck.java

---
+++ LongTruck.java
@@ -0,0 +1,10 @@
+public class LongTruck {
+    // most likely to use composition
+    private final Scania scania = new Scania(); // Composition
+
+    /*private final Scania parent;
+    public LongTruck(Scania parent) {
+        this.parent = parent;*/
+
+
+}
